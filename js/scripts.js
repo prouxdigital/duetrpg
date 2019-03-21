8fa0b9050b2ade408652bf1ddff29779701a60d7
Zach Allen <zallen@EDU-ZAllen-MBP.local>
2019-03-21 15:56:12 -0500

    added dice functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,45 @@
-;(function(window){
-	const playerEl = document.getElementById("player");
-	const gmEl = document.getElementById("gm");
+;(function(){
+	document.getElementById("rollDice").addEventListener("click", function() {
+		var el = document.getElementById("diceResult");
+		el.innerHTML = "<span class=\"icon\"><i class=\"fas fa-dice-d20 fa-pulse\"></i></span>";
+		setTimeout(function() {
+			el.innerHTML = rollDice();
+		}, 600);
+	});
 
+	function rollDice() {
+		var amt = Number(document.getElementById("diceAmt").value),
+			sides = Number(document.getElementById("diceSides").value),
+			mod = Number(document.getElementById("diceMod").value);
 
-})(window);
+		if(amt === 1 && mod === 0) {
+			return randomIntFromRange(1, sides);
+		} else {
+			var resultsArray = [],
+				total = 0;
+
+			for(var i = 0; i < amt; i++) {
+				let roll = randomIntFromRange(1, sides);
+				total += roll;
+				resultsArray.push(roll);
+			}
+			
+			if(mod === 0) {
+				return "<p class=\"help\">" + resultsArray.join(" + ") + "&nbsp;=</p>" + total;
+			} else if(mod < 0) {
+				total += mod;
+				return "<p class=\"help\">" + resultsArray.join(" + ") + " - " + Math.abs(mod) +  "&nbsp;=</p>" + total;
+			} else {
+				total += mod;
+				resultsArray.push(mod);
+				return "<p class=\"help\">" + resultsArray.join(" + ") + "&nbsp;=</p>" + total;
+			}
+		}
+	}
+
+	function randomIntFromRange(min,max) {
+		min = Math.ceil(min);
+    	max = Math.floor(max);
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+})();
